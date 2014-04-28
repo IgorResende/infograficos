904fd7cfd3a827667444b4fb7b4832a01bb60f6e
Luiz Tanure <letanure@gmail.com>
2014-04-28 04:15:54 -0300

    go to section & back behavior

--- source/assets/javascripts/arena-corinthians/init.js
+++ source/assets/javascripts/arena-corinthians/init.js
@@ -1,2 +1,39 @@
 'use strict'
-console.log('init')
+var infografico = (function() {
+  'use strict';
+
+  function init() {
+    $('.nav-section').on('click', function (evt) {
+      evt.preventDefault();
+      evt.stopPropagation();
+      var section = $(this).data('section');
+      $('#'+section).addClass('animated slideInUp').show();
+       var wait = window.setTimeout( function(){
+            $('#'+section).removeClass('slideInUp')}, 1300
+        );
+    });
+    bindBack();
+  }
+
+  function bindBack () {
+    $('.all-back').on('click', function (evt) {
+      evt.preventDefault();
+      evt.stopPropagation();
+      var $section = $(this).parents('.section');
+      $section.removeClass('slideInUp').addClass('slideOutDown')
+      var wait = window.setTimeout( function(){
+        $section.removeClass('slideOutDown')
+        $section.hide()
+      }, 1300 );
+    });
+  }
+
+  return {
+    init:init
+  };
+
+}());
+
+$(document).ready(function() {
+  infografico.init();
+});
