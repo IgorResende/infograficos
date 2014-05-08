41f929d4fd27a4d5d8d8eba62202dc56ab9c9a15
Luiz Tanure <letanure@gmail.com>
2014-05-08 13:15:51 -0300

    Automated commit at 2014-05-08 16:15:51 UTC by middleman-deploy 0.2.3

--- assets/javascripts/escalacao-selecao/init.js
+++ assets/javascripts/escalacao-selecao/init.js
@@ -4,7 +4,7 @@ var infografico = (function() {
 
   var playerSelected = null;
   var playerTypeSelected = null;
-  var imagePath: 'http://content.espn.com.br/infograficos/escalacao-selecao/'
+  var imagePath: 'http://content.espn.com.br/infograficos/escalacao-selecao/assets/images/'
   // var imagePath: 'http://0.0.0.0:4567/assets/images/'
 
   var schemaSelected = '4-3-3';
@@ -407,7 +407,7 @@ var infografico = (function() {
                  '<div class="tooltip-info">'+
                    '<div class="tooltip-info-name">' + disponiblePlayers[i].name + '</div>'+
                    '<div class="tooltip-info-games"><b>' + disponiblePlayers[i].games + '</b> <span>jogos pela seleção</span></div>'+
-                   '<div class="tooltip-info-goals"><b>' + disponiblePlayers[i].goals + '</b> <span>gols pela seleção</span></div>'+
+                   '<div class="tooltip-info-goals"><b>' + disponiblePlayers[i].goals + '</b> <span>' + (playerType == 1 ? 'gols softidos' : 'gols pela seleção') + '</span></div>'+
                    '<div class="tooltip-info-more"><a href="' + disponiblePlayers[i].link + '" target="_blank">Saiba mais</a></div>'+
                  '</div>'+
                '</div>'+
