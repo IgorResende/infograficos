1321a646ee6d11dd9fd03bcc8823fd5b91b569d5
Luiz Tanure <letanure@gmail.com>
2014-05-13 01:32:41 -0300

    include underscore

--- source/assets/javascripts/arena-corinthians.js
+++ source/assets/javascripts/arena-corinthians.js
@@ -1,4 +1,5 @@
 //= require _vendor/jquery.2.1.0.min
 //= require _vendor/jquery.queryloader2.min
 //= require _vendor/jquery.velocity.min
+//= require _vendor/underscore-min
 //= require arena-corinthians/init
